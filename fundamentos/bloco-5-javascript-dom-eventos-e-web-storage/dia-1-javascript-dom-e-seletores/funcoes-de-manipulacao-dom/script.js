7dd2446926e9525d87edc469870a09e8c1a895eb
Cauê Mustafá <cauemustafa@live.com>
2022-05-20 11:30:10 -0300

    Finalizando exercício: Funções de manipulação do DOM

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/funcoes-de-manipulacao-dom/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/funcoes-de-manipulacao-dom/script.js
@@ -12,3 +12,37 @@ Crie uma função que mude o texto na tag <p> para uma descrição de como você
  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
+
+const changeTextOnP = (text) => (document.getElementsByTagName('p')[0].innerText = text)
+
+changeTextOnP('loremipsum')
+
+// const changeMainContent = (backgroundColor) => (document.getElementsByClassName('.main-content')[0].style.backgroundColor = backgroundColor)
+
+const changeMainContent = (backgroundColor) => (document.getElementsByClassName('main-content')[0].style.backgroundColor = backgroundColor)
+
+changeMainContent('green')
+
+const changeCenterContent = (backgroundColor) => (document.getElementsByClassName('center-content')[0].style.backgroundColor = backgroundColor)
+
+changeCenterContent('white')
+
+const correctText = (text) => (document.getElementsByClassName('title')[0].innerText = text)
+
+correctText('JavaScript')
+
+const changeToUpperCase = () => {
+  const paragraph = document.getElementsByTagName('p')
+  for (let key = 0; key < paragraph.length; key += 1) {
+    paragraph[key].style.textTransform = 'uppercase'
+  }
+}
+changeToUpperCase()
+
+const showTags = () => {
+  const tags = document.getElementsByTagName('p')
+  for (const tag of tags) {
+    console.log(tag.innerHTML)
+  }
+}
+showTags()
