06b621baed7307f0c953a4c7609b6f148aeaf944
Cauê Mustafá <cauemustafa@live.com>
2022-05-20 11:44:37 -0300

    Finalizando (parte 2) exercício: Funções de manipulação do DOM

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/funcoes-de-manipulacao-dom/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/funcoes-de-manipulacao-dom/script.js
@@ -5,19 +5,17 @@ Aqui você vai modificar os elementos já existentes utilizando apenas as funç�
 - document.getElementsByClassName()
 - document.getElementsByTagName()
 
-Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
- Crie uma função que mude a cor do quadrado vermelho para branco.
- Crie uma função que corrija o texto da tag <h1>.
- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
- Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+1. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+3. Crie uma função que mude a cor do quadrado vermelho para branco.
+4. Crie uma função que corrija o texto da tag <h1>.
+5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+6. Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
 
 const changeTextOnP = (text) => (document.getElementsByTagName('p')[0].innerText = text)
 
 changeTextOnP('loremipsum')
-
-// const changeMainContent = (backgroundColor) => (document.getElementsByClassName('.main-content')[0].style.backgroundColor = backgroundColor)
 
 const changeMainContent = (backgroundColor) => (document.getElementsByClassName('main-content')[0].style.backgroundColor = backgroundColor)
 
@@ -33,8 +31,8 @@ correctText('JavaScript')
 
 const changeToUpperCase = () => {
   const paragraph = document.getElementsByTagName('p')
-  for (let key = 0; key < paragraph.length; key += 1) {
-    paragraph[key].style.textTransform = 'uppercase'
+  for (const p of paragraph) {
+    p.style.textTransform = 'uppercase'
   }
 }
 changeToUpperCase()
