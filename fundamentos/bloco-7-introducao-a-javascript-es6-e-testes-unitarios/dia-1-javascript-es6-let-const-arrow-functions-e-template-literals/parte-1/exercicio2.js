a24c56f7511c60203a821c615f9c79427601ae77
Cauê Mustafá <cauemustafa@live.com>
2022-06-08 16:14:49 -0300

    Parte 1 - Ex.2 - adicionando função que retorna ordem aleatória com Fisher Yates Method

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1/exercicio2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1/exercicio2.js
@@ -7,11 +7,24 @@ Spoiler: É possível realizar uma função que ordene qualquer array de número
 const oddsAndEvens = [13, 3, 4, 10, 7, 2]
 console.log('Ordem original: ' + oddsAndEvens)
 
-oddsAndEvens.sort((a, b) => a - b)
-console.log(`Ordem crescente: ${oddsAndEvens}`)
+// array.sort() with compare function
+console.log(`Ordem crescente: ${oddsAndEvens.sort((a, b) => a - b)}`)
 
-oddsAndEvens.sort((a, b) => b - a)
-console.log(`Ordem decrescente: ${oddsAndEvens}`)
+console.log(`Ordem decrescente: ${oddsAndEvens.sort((a, b) => b - a)}`)
+
+// The Fisher Yates Method
+// Random Order
+const randomOrder = (arr) => {
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * i)
+    const k = arr[i]
+    arr[i] = arr[j]
+    arr[j] = k
+  }
+  return arr
+}
+
+console.log(`Ordem aleatória: ${randomOrder(oddsAndEvens)}`)
 
 // reference:
 // https://www.w3schools.com/js/js_array_sort.asp
