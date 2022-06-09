390c8a846144cae0dbf39fbcfeb8d34611a68b3d
Cauê Mustafá <cauemustafa@live.com>
2022-06-09 01:23:45 -0300

    organizando referências do Ex.1 - Parte 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-2/exercicio-1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-2/exercicio-1.js
@@ -3,8 +3,6 @@ Na matemática, o fatorial de um número não negativo N, com a notação N!, é
 
 Bônus (opcional): tente fazer o mesmo exercício de forma recursiva.
 
-http://devfuria.com.br/logica-de-programacao/recursividade-fatorial/
-
 Spoiler: É possível resolver com uma linha usando ternary operator. */
 
 const calculaFatorial = (number) => {
@@ -15,6 +13,10 @@ const calculaFatorial = (number) => {
 console.log('Modelo 1: ' + calculaFatorial(4))
 
 // de forma não recursiva
+/* references:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Decrement
+http://devfuria.com.br/logica-de-programacao/recursividade-fatorial/
+*/
 const calculaFatorial2 = (number) => {
   let fatorial = number
   if (number === 0) return 1
@@ -22,7 +24,6 @@ const calculaFatorial2 = (number) => {
   return fatorial
 }
 console.log('Modelo 2: ' + calculaFatorial2(4))
-// reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Decrement
 
 // resolvendo com operador ternário usando a forma recursiva
 // reference: https://www.ime.usp.br/~leo/mac2166/2017-1/introducao_recursividade_exemplos.html
